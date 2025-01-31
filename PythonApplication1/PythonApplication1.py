d68e4109f8acb2aac26b5bed1948cfff1eeca83c
RemDebav <165015133+RemDebav@users.noreply.github.com>
2025-01-31 16:28:08 +0100

    Update PythonApplication1.py

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,7 +1,5 @@
 t=open("tiny.txt","r").read()
 
-
-
 def lecture_traitement(f):
     l=open(f,"r").read()
     return l.lower()
@@ -37,19 +35,22 @@ def liste_occurrences(l):
             res[i]+=1
     return res
 
+tinyy=liste_occurrences(tiny)
 
+def reduction_occurence_unique(d):
+    res=dict()
+    for i in d:
+        if not d[i]==1:
+            res[i]=d[i]
+    return res
 
+tinyyy=reduction_occurence_unique(tinyy)
+
+def reduction_occurence_100(d):
+    ens=set()
+    for var in d:
+        ens.append
   
 
             
-            
-        
-        
-    
-
-
-    
-    
-    
-
-
+l=[["baa",4],["a",1],["baba",2],["bba",3]]   
