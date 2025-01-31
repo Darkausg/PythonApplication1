d491afe703046dfd404e0f51b443b791a9756683
RemDebav <165015133+RemDebav@users.noreply.github.com>
2025-01-31 15:52:04 +0100

    Update PythonApplication1.py

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,5 +1,53 @@
-print("okay")
-print("I am testing something And I AM Happy")
+f=open("tiny.txt","r").read()
 
-def main():
-    return 0
+def lecture_traitement(f):
+    pass
+    
+
+def only_caracter(ori_text):
+    letter ="azertyuiopqsdfghjklmwxcvbn"
+    text_treated = ""
+    for i in ori_text:
+        if i in letter:
+            text_treated = text_treated + i
+        else:
+            text_treated = text_treated + " "
+    return text_treated
+
+only_caracter(f)
+
+
+def liste_occurrences(l):
+    res=dict()
+    print(l)
+    print("l")
+    for i in l:
+        if not i in res:
+            res[i]=1
+        else:
+            res[i]+=1
+    return res
+
+
+def parser(f):
+    ori_text = lecture_traitement(f)
+    text_treated = only_caracter(ori_text)
+    text_treated.split()
+    print(text_treated)
+    occurence = liste_occurrences(text_treated)
+    print(occurence)
+
+  
+
+            
+            
+        
+        
+    
+
+
+    
+    
+    
+
+
